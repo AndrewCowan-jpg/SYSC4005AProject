8f061f6b4d0605987c6327610d46e993a568a6b8
MichaelEvans6 <michael3vans66@gmail.com>
2021-03-23 23:56:22 -0400

    added stuff

--- Milestone3/Simulation.py
+++ Milestone3/Simulation.py
@@ -1,5 +1,18 @@
 from enum import Enum
+import csv
 
+with open('RandomTimes/W1.csv', newline='') as csvfile:
+    W1 = list(csv.reader(csvfile))
+with open('RandomTimes/W2.csv', newline='') as csvfile:
+    W2 = list(csv.reader(csvfile))
+with open('RandomTimes/W3.csv', newline='') as csvfile:
+    W3 = list(csv.reader(csvfile))
+with open('RandomTimes/C1.csv', newline='') as csvfile:
+    C1 = list(csv.reader(csvfile))
+with open('RandomTimes/C2.csv', newline='') as csvfile:
+    C2 = list(csv.reader(csvfile))
+with open('RandomTimes/C3.csv', newline='') as csvfile:
+    C3 = list(csv.reader(csvfile))
 
 class States(Enum):
     WAITING = 0
